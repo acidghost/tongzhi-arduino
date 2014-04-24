544dcd50f8077728c02769dbbf64d04cf38975e8
Andrea Jemmett <andreajemmett@gmail.com>
2014-04-24 10:38:55 +0200

    Blink a LED counting unseen FB likes

--- main.ino
+++ main.ino
@@ -1,9 +1,21 @@
 /*
- * 
+ * This is the Arduino side of tongzhi
+ *
+ * It will receive messages from the node.js notifier
+ * and take some actions accordingly
 */
+
+const int fbLikesLed = 13;
+
+const String fbLikesMsg = "FB-likes ";
+
+unsigned int fbLikes = 0;
+unsigned int fbLikesCount = 0;
 
 void setup() {
 	Serial.begin(9600);
+
+	pinMode(fbLikesLed, OUTPUT);
 }
 
 void loop() {
@@ -18,7 +30,24 @@ void loop() {
 	}
 
 	if(line != "") {
-		Serial.println("Back: "+line);
+		Serial.println("Received: "+line);
+
+		if(line.indexOf(fbLikesMsg) == 0) {
+			fbLikes = line.substring(fbLikesMsg.length(), line.length()).toInt();
+			fbLikesCount = fbLikes;
+		}
 	}
+
+	if(fbLikes > 0 && fbLikesCount > 0) {
+		digitalWrite(fbLikesLed, HIGH);
+	}
+
 	delay(500);
+
+	if(fbLikes > 0 && fbLikesCount > 0) {
+		digitalWrite(fbLikesLed, LOW);
+		fbLikesCount--;
+	}
+
+	delay(250);
 }
